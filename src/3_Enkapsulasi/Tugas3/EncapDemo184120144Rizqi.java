ff7d4b5f58aca38ee78d175e3fa1b951ca7c7244
mahe62 <mahee6232@gmail.com>
2019-09-14 14:25:26 +0700

    src t1,2,3

--- src/3_Enkapsulasi/Tugas3/EncapDemo184120144Rizqi.java
+++ src/3_Enkapsulasi/Tugas3/EncapDemo184120144Rizqi.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Tugas1;
+package Tugas3;
 
 /**
  *
@@ -26,6 +26,8 @@ public class EncapDemo184120144Rizqi {
         if (newAge>30) {
             mAge = 30;
             
+        }else if (newAge<18) {
+            mAge = 18;
         }else{
             mAge =newAge;
         }
--- src/3_Enkapsulasi/Tugas3/EncapTest1841720144Rizqi.java
+++ src/3_Enkapsulasi/Tugas3/EncapTest1841720144Rizqi.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Tugas1;
+package Tugas3;
 
 /**
  *
@@ -12,13 +12,20 @@ package Tugas1;
 public class EncapTest1841720144Rizqi {
     public static void main(String[] args) {
         EncapDemo184120144Rizqi encap = new EncapDemo184120144Rizqi();
-    
+        EncapDemo184120144Rizqi encap1 = new EncapDemo184120144Rizqi();
+        
         encap.SetNamaeRizqi("James");
         encap.SetAgeRizqi(35);
+        
+        encap1.SetNamaeRizqi("Rodrigez");
+        encap1.SetAgeRizqi(17);
         
         System.out.println("Nama : "+encap.GetNameRizqi());
         System.out.println("Nama : "+encap.GetAgeRizqi());
         System.out.println("");
+        
+        System.out.println("Nama : "+encap1.GetNameRizqi());
+        System.out.println("Nama : "+encap1.GetAgeRizqi());
     }
    
 }
